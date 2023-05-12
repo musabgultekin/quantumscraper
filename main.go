f17432a1fb3e84af8c60692e6b6490573131b020
Musab Gültekin <gultekinmusab@gmail.com>
2023-05-12 13:17:42 +0300

    Main scraper entrypoint

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"github.com/musabgultekin/quantumscraper/domains"
+	"github.com/musabgultekin/quantumscraper/storage"
+	"github.com/nsqio/go-nsq"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Println("failed", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	storage.StartNSQDEmbeddedServer()
+
+	var err error
+	consumer, err := startConsumer()
+	if err != nil {
+		return fmt.Errorf("consumer: %w", err)
+	}
+
+	//visitedURLStorage, err := storage.NewVisitedURLStorage(path.Join(os.TempDir(), "visited_urls"))
+	//if err != nil {
+	//	return fmt.Errorf("visited url storage creation: %w", err)
+	//}
+
+	producer, err := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+	if err != nil {
+		return fmt.Errorf("nsq new producer: %w", err)
+	}
+
+	domainLoader, err := domains.NewDomainLoader()
+	if err != nil {
+		return fmt.Errorf("domain loader: %w", err)
+	}
+
+	for {
+		domain, err := domainLoader.NextDomain()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return fmt.Errorf("next domain: %w", err)
+		}
+
+		if err := producer.Publish("topic", []byte("https://"+domain)); err != nil {
+			return fmt.Errorf("producer publish: %w", err)
+		}
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	// Gracefully stop the consumer.
+	consumer.Stop()
+
+	return nil
+}
+
+// startConsumer start consumer and wait for messages
+func startConsumer() (*nsq.Consumer, error) {
+	consumer, err := nsq.NewConsumer("topic", "channel", nsq.NewConfig())
+	if err != nil {
+		return nil, fmt.Errorf("nsq new consumer: %w", err)
+	}
+
+	consumer.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		fmt.Println(string(message.Body))
+		return nil
+	}))
+
+	if err := consumer.ConnectToNSQD("localhost:4150"); err != nil {
+		return nil, fmt.Errorf("connect to nsqd: %w", err)
+	}
+
+	return consumer, nil
+}
